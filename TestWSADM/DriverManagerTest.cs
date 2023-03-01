62aef5b28e8da1cd943751cd753ebaea2ac49624
Wjinlei <freebufer.wang@gmail.com>
2023-03-01 10:00:15 +0800

    test: Add DriverManagerTest class.

---
+++ TestWSADM/DriverManagerTest.cs
@@ -0,0 +1,13 @@
+﻿namespace TestWSADM;
+
+public class DriverManagerTest
+{
+    [Test]
+    public void TestDriverManager()
+    {
+        var result = WSADM.DriverManager.RegisterDriver("Driver.IIS", "IIS.dll");
+        if (!result.Success)
+            TestContext.Out.WriteLine(result.Message);
+        Assert.That(result.Success, Is.True);
+    }
+}
