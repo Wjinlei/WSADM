9270d14e8f9466dcfbaae48155232ee7ca4fcce3
Wjinlei <freebufer.wang@gmail.com>
2023-03-03 10:17:15 +0800

    refactor: Optimize DriverManagerTest.

--- TestWSADM/DriverManagerTest.cs
+++ TestWSADM/DriverManagerTest.cs
@@ -5,9 +5,20 @@ public class DriverManagerTest
     [Test]
     public void TestDriverManager()
     {
-        var result = WSADM.DriverManager.RegisterDriver("Driver.IIS", "IIS.dll");
-        if (!result.Success)
-            TestContext.Out.WriteLine(result.Message);
-        Assert.That(result.Success, Is.True);
+        // Get Driver
+        var driverResult = WSADM.DriverManager.RegisterDriver("Driver.Apache", "Driver.dll");
+        Assert.That(driverResult.Success, Is.True);
+
+        // Get Server
+        var driver = driverResult.GetOk();
+        var serverResult = driver.GetServer("/var/hwsmaster/soft/server/apache_2.4.26");
+        Assert.That(serverResult.Success, Is.True);
+
+        // Do something
+        var apache = serverResult.GetOk();
+        apache.Start();
+        apache.Stop();
+        apache.Restart();
+        apache.Reload();
     }
 }
