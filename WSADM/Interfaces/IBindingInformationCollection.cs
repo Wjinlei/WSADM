fdecd18080cbda7226e640fa3c3e644a21667d49
Wjinlei <freebufer.wang@gmail.com>
2023-03-07 12:02:41 +0800

    refactor: Optimize return type.

--- WSADM/Interfaces/IBindingInformationCollection.cs
+++ WSADM/Interfaces/IBindingInformationCollection.cs
@@ -2,7 +2,7 @@
 
 public interface IBindingInformationCollection
 {
-    public Result Add(IBindingInformation bindingInformation);
+    public Result<IBindingInformation> Add(IBindingInformation bindingInformation);
     public Result<IBindingInformation> Add(string domain);
     public Result<IBindingInformation> Add(int port);
     public Result<IBindingInformation> Add(string domain, int port);
--- WSADM/Interfaces/ISiteCollection.cs
+++ WSADM/Interfaces/ISiteCollection.cs
@@ -2,7 +2,7 @@
 
 public interface ISiteCollection
 {
-    public Result Add(ISite site);
+    public Result<ISite> Add(ISite site);
     public Result<ISite> Add(string name, string physicalPath, string domain);
     public Result<ISite> Add(string name, string physicalPath, int port);
     public Result<ISite> Add(string name, string physicalPath, string domain, int port);
