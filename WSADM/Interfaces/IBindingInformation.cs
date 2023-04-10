be50103a00249fa2d535db91b2a481ad57c63a15
Wjinlei <freebufer.wang@gmail.com>
2023-04-10 16:50:15 +0800

    refactor: Optimize

--- WSADM/Interfaces/IBindingInformation.cs
+++ WSADM/Interfaces/IBindingInformation.cs
@@ -6,5 +6,5 @@ public interface IBindingInformation
     public int Port { get; set; }
 
     public string Format(); // Example for iis: *:8080:www.example.com,  127.0.0.1:8080:www.example.com
-    public string SimpleFormat(); // Example for iis: www.example.com:8080
+    public string FormatSimple(); // Example for iis: www.example.com:8080
 }
