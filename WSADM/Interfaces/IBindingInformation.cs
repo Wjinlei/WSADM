9961b4faea45e7cb9ea713dcd9d413038b57ded4
Wjinlei <freebufer.wang@gmail.com>
2023-04-18 09:56:40 +0800

    refactor: Modify the method to the property

--- WSADM/Interfaces/IBindingInformation.cs
+++ WSADM/Interfaces/IBindingInformation.cs
@@ -2,10 +2,10 @@
 
 public interface IBindingInformation
 {
-    public string Domain { get; set; }
+    public string DomainName { get; set; }
     public int Port { get; set; }
     public string IpAddr { get; set; }
 
-    public string ToBind(); // Example for iis: *:8080:www.example.com,  127.0.0.1:8080:www.example.com
-    public string ToUrl(); // Example for iis: www.example.com:8080
+    public string EndPoint { get; } //Example: www.example.com:8080
+    public string Host { get; } // Example: *:8080:www.example.com,  127.0.0.1:8080:www.example.com
 }
