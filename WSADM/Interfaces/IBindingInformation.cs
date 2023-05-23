7fbf2f0e1e81438391179142b0cbb21dc6bf88eb
Wjinlei <freebufer.wang@gmail.com>
2023-05-23 15:48:29 +0800

    refactor: EndPoint rename to HostPort.

--- WSADM/Interfaces/IBindingInformation.cs
+++ WSADM/Interfaces/IBindingInformation.cs
@@ -3,9 +3,8 @@
 public interface IBindingInformation
 {
     public string Address { get; set; }
-    public int Port { get; set; }
     public string Host { get; set; }
-
-    public string BindingInformation { get; } // Example: *:8080:www.example.com, 127.0.0.1:8080:www.example.com
-    public string EndPoint { get; } // Example: www.example.com:8080
+    public int Port { get; set; }
+    public string BindingInformation { get; }
+    public string HostPort { get; }
 }
