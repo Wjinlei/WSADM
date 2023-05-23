270f462e9280d4bce1bcb5205c97112308760161
Wjinlei <freebufer.wang@gmail.com>
2023-05-23 15:49:24 +0800

    refactor: Reorder code.

--- WSADM/Interfaces/IBindingInformationCollection.cs
+++ WSADM/Interfaces/IBindingInformationCollection.cs
@@ -6,18 +6,18 @@ public interface IBindingInformationCollection : IEnumerable<IBindingInformation
     public IBindingInformation this[int index] { get; }
     public int Count { get; }
 
-    // Method to implement the List data structure
-    public void Remove(string bindingInformation);
-    public void Remove(IBindingInformation bindingInformation);
-    public void Clear();
-    public bool Contains(string bindingInformation);
-    public bool Contains(IBindingInformation bindingInformation);
-    public IBindingInformation? Find(Predicate<IBindingInformation> match);
-
-    // Special methods
+    // Specific methods
     public Result Add(IBindingInformation bindingInformation);
     public Result Add(int port);
     public Result Add(string domain, int port);
     public Result Add(string ipAddr, string domain, int port);
     public Result Add(string bindingInformation);
+
+    // Some methods to implement the List data structure
+    public void Clear();
+    public void Remove(string bindingInformation);
+    public void Remove(IBindingInformation bindingInformation);
+    public bool Contains(string bindingInformation);
+    public bool Contains(IBindingInformation bindingInformation);
+    public IBindingInformation? Find(Predicate<IBindingInformation> match);
 }
--- WSADM/Interfaces/ISiteCollection.cs
+++ WSADM/Interfaces/ISiteCollection.cs
@@ -3,22 +3,22 @@
 public interface ISiteCollection<ISite> : IEnumerable<ISite>
 {
     public ISite? this[string name] { get; }
-    public ISite this[int index] { get; } 
+    public ISite this[int index] { get; }
     public int Count { get; }
 
-    // Method to implement the List data structure
-    public void Remove(string name);
-    public void Remove(ISite site);
-    public void Clear();
-    public bool Contains(string name);
-    public bool Contains(ISite site);
-    public ISite? Find(Predicate<ISite> match);
-
-    // Special methods
+    // Specific methods
     public Result Add(ISite site);
     public Result Add(string name, string physicalPath, IBindingInformationCollection bindings);
     public Result Add(string name, string physicalPath, int port);
     public Result Add(string name, string physicalPath, string domain, int port);
     public Result Add(string name, string physicalPath, string binding);
     public Result Add(string name, string physicalPath, List<string> bindings);
+
+    // Some methods to implement the List data structure
+    public void Clear();
+    public void Remove(string name);
+    public void Remove(ISite site);
+    public bool Contains(string name);
+    public bool Contains(ISite site);
+    public ISite? Find(Predicate<ISite> match);
 }
